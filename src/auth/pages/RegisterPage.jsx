00d6cac667821b5438733cef1a24eca2af3b264b
Christian <chrisdaq@gmail.com>
2024-01-18 15:29:51 -0400

    add register page using the auth layout

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,3 +1,56 @@
+import { Google } from "@mui/icons-material";
+import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import { AuthLayout } from "../layout/AuthLayout";
+
 export const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  return (
+    <AuthLayout title="Register">
+      <form>
+        <Grid container>
+          <Grid item xs={12} md={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Name"
+              type="text"
+              placeholder="John Doe"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} md={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Email"
+              type="email"
+              placeholder="email@google.com"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item xs={12} md={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Password"
+              type="password"
+              placeholder="password"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={12} sm={6}>
+              <Button variant="contained" fullWidth>
+                Add account
+              </Button>
+            </Grid>
+          </Grid>
+
+          <Grid container direction=" row" justifyContent="end">
+            <Typography sx={{ mr: 1 }}>Have already an account?</Typography>
+            {/* use react-router-dom with material link*/}
+            <Link component={RouterLink} color="inherit" to="/auth/login">
+              Login
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
+  );
 };
