79e6ea7830fef3f2aaf763bf429eb91d46349914
Christian <chrisdaq@gmail.com>
2024-01-19 11:47:24 -0400

    add navbar for journal components, load it on Journal layout

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,11 +1,12 @@
 import { Box } from "@mui/material";
+import { NavBar } from "../components";
 
 const drawerWidth = 240;
 
 export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: "flex" }}>
-      {/* navbar */}
+      <NavBar drawerWidth={drawerWidth} />
       {/* sidebar */}
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         {/** toolbar */}
