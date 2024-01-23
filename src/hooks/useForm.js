3fdd18854cf74df29e06fc4ada49c9cac2c126a8
Christian <chrisdaq@gmail.com>
2024-01-23 12:25:30 -0400

    add useForm custom hook

---
+++ src/hooks/useForm.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export const useForm = (initialValue) => {
+  const [formState, setFormState] = useState(initialValue);
+
+  const onInputChange = ({ target }) => {
+    const { name, value } = target;
+    setFormState({
+      ...formState,
+      [name]: value, // computed property
+    });
+  };
+  const onResetForm = () => {
+    setFormState(initialValue);
+  };
+
+  return {
+    ...formState,
+    formState,
+    onInputChange,
+    onResetForm,
+  };
+};
