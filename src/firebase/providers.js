76d2dbad3282e57d4c775ac25177e8b60912ba5a
Christian <chrisdaq@gmail.com>
2024-01-24 15:29:09 -0400

    add provider for google signin, retrieve it in thunks

---
+++ src/firebase/providers.js
@@ -0,0 +1,28 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { firebaseAuth } from "./config";
+
+const googleProvider = new GoogleAuthProvider();
+
+export const signInWithGoogle = async () => {
+  try {
+    const result = await signInWithPopup(firebaseAuth, googleProvider);
+    // const credentials = GoogleAuthProvider.credentialFromResult(result)// get credentials
+    const { displayName, email, photoURL, uid } = result.user;
+    return {
+      ok: true,
+      displayName,
+      email,
+      photoURL,
+      uid,
+    };
+  } catch (error) {
+    console.log(error);
+    const errorMessage = error.message;
+    // const email = error.customData.email;
+    // const credential = GoogleAuthProvider.credentialFromError(error);
+    return {
+      ok: false,
+      message: errorMessage,
+    };
+  }
+};
