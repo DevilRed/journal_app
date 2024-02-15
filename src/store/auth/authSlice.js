f7587cd82be42c1c132945ae27fe9557d1bad344
Christian <chrisdaq@gmail.com>
2024-02-15 11:45:30 -0400

    set state on login, logout actions in authSlice, send error message in signin fails

--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -11,9 +11,25 @@ export const authSlice = createSlice({
     errorMessage: null,
   },
   reducers: {
-    login: (state, action) => {},
-    logout: (state, payload) => {},
+    login: (state, { payload }) => {
+      state.status = "authenticated";
+      state.uid = payload.uid;
+      state.email = payload.email;
+      state.displayName = payload.displayName;
+      state.photoURL = payload.photoURL;
+      state.errorMessage = null;
+    },
+    logout: (state, { payload }) => {
+      // return to initial state
+      state.status = "not-authenticated";
+      state.uid = null;
+      state.email = null;
+      state.displayName = null;
+      state.photoURL = null;
+      state.errorMessage = payload.errorMessage;
+    },
     checkingCredentials: (state) => {
+      console.log(state);
       state.status = "checking";
     },
   },
