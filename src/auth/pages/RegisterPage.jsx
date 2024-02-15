50356d54c19a3c9a7855f3e928552a2dfcad4990
Christian <chrisdaq@gmail.com>
2024-02-15 17:30:20 -0400

    use custom hook to get form values on register page

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,12 +1,25 @@
-import { Google } from "@mui/icons-material";
 import { Button, Grid, Link, TextField, Typography } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
 import { AuthLayout } from "../layout/AuthLayout";
+import { useForm } from "../../hooks/useForm";
+
+const formData = {
+  email: "thulio@hinojosa.com",
+  password: "123456",
+  displayName: "Thulio Tony",
+};
 
 export const RegisterPage = () => {
+  const { displayName, email, password, onInputChange, formState } =
+    useForm(formData);
+
+  const onSubmit = (ev) => {
+    ev.preventDefault();
+    console.log(formState);
+  };
   return (
     <AuthLayout title="Register">
-      <form>
+      <form onSubmit={onSubmit}>
         <Grid container>
           <Grid item xs={12} md={12} sx={{ mt: 2 }}>
             <TextField
@@ -14,6 +27,9 @@ export const RegisterPage = () => {
               type="text"
               placeholder="John Doe"
               fullWidth
+              name="displayName"
+              value={displayName}
+              onChange={onInputChange}
             />
           </Grid>
           <Grid item xs={12} md={12} sx={{ mt: 2 }}>
@@ -22,6 +38,9 @@ export const RegisterPage = () => {
               type="email"
               placeholder="email@google.com"
               fullWidth
+              name="email"
+              value={email}
+              onChange={onInputChange}
             />
           </Grid>
 
@@ -31,18 +50,21 @@ export const RegisterPage = () => {
               type="password"
               placeholder="password"
               fullWidth
+              name="password"
+              value={password}
+              onChange={onInputChange}
             />
           </Grid>
 
           <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
             <Grid item xs={12} sm={6}>
-              <Button variant="contained" fullWidth>
+              <Button variant="contained" fullWidth type="submit">
                 Add account
               </Button>
             </Grid>
           </Grid>
 
-          <Grid container direction=" row" justifyContent="end">
+          <Grid container direction="row" justifyContent="end">
             <Typography sx={{ mr: 1 }}>Have already an account?</Typography>
             {/* use react-router-dom with material link*/}
             <Link component={RouterLink} color="inherit" to="/auth/login">
