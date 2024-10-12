b4d6e35724fc646a8d29fdb58a7caafebbbca16e
Christian <chrisdaq@gmail.com>
2024-10-12 17:33:13 -0400

    add custom validations through createValidators, loop through formValidations, use state formValidation to redraw things if validations go wrong, check validation on useEffect every time state is changed

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,7 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-export const useForm = (initialValue) => {
+export const useForm = (initialValue = {}, formValidations = {}) => {
   const [formState, setFormState] = useState(initialValue);
+  const [formValidation, setFormValidation] = useState({});
+  useEffect(() => {
+    createValidators();
+  }, [formState]);
 
   const onInputChange = ({ target }) => {
     const { name, value } = target;
@@ -14,10 +18,27 @@ export const useForm = (initialValue) => {
     setFormState(initialValue);
   };
 
+  const createValidators = () => {
+    const formCheckedValues = {};
+
+    // loop through formValidations keys
+    for (const formField of Object.keys(formValidations)) {
+      // get validator function, message of formValidations[key]
+      const [fn, errorMessage] = formValidations[formField];
+      // add, construct object property of formCheckedValues as it's set on LoginPage useForm
+      formCheckedValues[`${formField}Valid`] = fn(formState[formField])
+        ? null
+        : errorMessage;
+    }
+    // set state form validation
+    setFormValidation(formCheckedValues);
+  };
+
   return {
     ...formState,
     formState,
     onInputChange,
     onResetForm,
+    ...formValidation,
   };
 };
