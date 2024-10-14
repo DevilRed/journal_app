077a7212fc7dbb61a06f356036c273cb3d91ed31
Christian <chrisdaq@gmail.com>
2024-10-14 12:03:56 -0400

    add auth thunk to create user with email, password

--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,4 +1,7 @@
-import { signInWithGoogle } from "../../firebase/providers";
+import {
+  signInWithGoogle,
+  registerUserWithEmailPassword,
+} from "../../firebase/providers";
 import { checkingCredentials, login, logout } from "./authSlice";
 
 export const checkingAuthentication = (email, password) => {
@@ -15,5 +18,21 @@ export const startGoogleSignIn = () => {
     if (!result.ok) return dispatch(logout(result.errorMessage));
 
     dispatch(login(result));
+  };
+};
+
+export const startCreatingUserWithEmailPassword = ({
+  email,
+  password,
+  displayName,
+}) => {
+  return async (dispatch) => {
+    dispatch(checkingCredentials());
+    const resp = registerUserWithEmailPassword({
+      email,
+      password,
+      displayName,
+    });
+    console.log(resp);
   };
 };
