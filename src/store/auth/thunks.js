cab256d2c7b1ebf48007481f06016c3888476b66
Christian <chrisdaq@gmail.com>
2024-10-17 15:53:25 -0400

    use new provider on thunk function

--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,6 +1,7 @@
 import {
   signInWithGoogle,
   registerUserWithEmailPassword,
+  loginUserWithEmailPassword,
 } from "../../firebase/providers";
 import { checkingCredentials, login, logout } from "./authSlice";
 
@@ -33,6 +34,19 @@ export const startCreatingUserWithEmailPassword = ({
       password,
       displayName,
     });
+    if (!ok) return dispatch(logout({ message }));
+    dispatch(login({ uid, email, displayName, photoURL }));
+  };
+};
+
+export const startLoginWithEmailPassword = ({ email, password }) => {
+  return async (dispatch) => {
+    dispatch(checkingCredentials());
+    const { ok, uid, photoURL, message, displayName } =
+      await loginUserWithEmailPassword({
+        email,
+        password,
+      });
     if (!ok) return dispatch(logout({ message }));
     dispatch(login({ uid, email, displayName, photoURL }));
   };
