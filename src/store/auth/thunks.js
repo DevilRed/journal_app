ffb4030baf6ec17c8628b128149ce0b9d9e3fe30
Christian <chrisdaq@gmail.com>
2024-10-21 15:43:45 -0400

    call provider from auth thunks

--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -2,6 +2,7 @@ import {
   signInWithGoogle,
   registerUserWithEmailPassword,
   loginUserWithEmailPassword,
+  logoutFirebase,
 } from "../../firebase/providers";
 import { checkingCredentials, login, logout } from "./authSlice";
 
@@ -49,5 +50,16 @@ export const startLoginWithEmailPassword = ({ email, password }) => {
       });
     if (!ok) return dispatch(logout({ message }));
     dispatch(login({ uid, email, displayName, photoURL }));
+  };
+};
+
+export const startLogout = () => {
+  return async (dispatch) => {
+    try {
+      await logoutFirebase();
+      dispatch(logout()); // from auth slice
+    } catch (error) {
+      console.log(error);
+    }
   };
 };
