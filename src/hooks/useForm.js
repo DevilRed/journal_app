145ab6817b11bb46e249378263b97b681c025d4a
Christian <chrisdaq@gmail.com>
2024-10-28 11:25:48 -0400

    update values on useForm when their initial value change

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -6,6 +6,10 @@ export const useForm = (initialValue = {}, formValidations = {}) => {
   useEffect(() => {
     createValidators();
   }, [formState]);
+  useEffect(() => {
+    setFormState(initialValue);
+  }, [initialValue]);
+
   const isFormValid = useMemo(() => {
     for (const formValue of Object.keys(formValidation)) {
       if (formValidation[formValue] !== null) return false;
