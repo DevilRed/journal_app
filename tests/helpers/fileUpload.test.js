343f69e0301b51fe2084bac7d4543de6f7bbe7fd
Christian <chrisdaq@gmail.com>
2024-10-31 17:53:14 -0400

    delete uploaded image used in tests

--- tests/helpers/fileUpload.test.js
+++ tests/helpers/fileUpload.test.js
@@ -1,4 +1,11 @@
 import { fileUpload } from "../../src/helpers/fileUpload";
+import { v2 as cloudinary } from "cloudinary";
+cloudinary.config({
+  cloud_name: "dukewoyh4",
+  api_key: "696446912682682",
+  api_secret: "SK_NbG0-1lNYACUg0-Z8bm3nE1g",
+  secure: true,
+});
 
 describe("fileUpload", () => {
   it("should upload files to cloudinary", async () => {
@@ -11,5 +18,18 @@ describe("fileUpload", () => {
 
     const url = await fileUpload(file);
     expect(typeof url).toBe("string");
+
+    // delete img from cloudinary
+    const segments = url.split("/");
+    // get the last segment where img id is
+    const imageId = segments[segments.length - 1].replace(".jpg", "");
+    await cloudinary.api.delete_resources([imageId]);
+  });
+
+  it("should return null", () => {
+    const file = new File([], "babe.jpg");
+    expect(async () => {
+      await fileUpload(file);
+    }).rejects.toThrow();
   });
 });
