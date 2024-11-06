53aaf3a43999274af116af60cd1c1409591ad68a
Christian <chrisdaq@gmail.com>
2024-11-06 14:58:18 -0400

    move env variables to proper files and load them in firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,26 +4,24 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore/lite";
 import { getEnvironments } from "../helpers";
 // TODO: Add SDKs for Firebase products that you want to use
-const env = getEnvironments();
-console.log(env);// 8.21
+const {
+  VITE_APIKEY,
+  VITE_AUTHDOMAIN,
+  VITE_PROJECTID,
+  VITE_STORAGEBUCKET,
+  VITE_MESSAGINGSENDERID,
+  VITE_APPID,
+} = getEnvironments();
+
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-/* const firebaseConfig = {
-  apiKey: "AIzaSyDdkYdWcDyP79ZAzu18LWtg243kyFK7oGM",
-  authDomain: "react-journal-app-613a8.firebaseapp.com",
-  projectId: "react-journal-app-613a8",
-  storageBucket: "react-journal-app-613a8.appspot.com",
-  messagingSenderId: "481904725054",
-  appId: "1:481904725054:web:b075236bcf4978226c956f",
-}; */
 const firebaseConfig = {
-  apiKey: "AIzaSyAaykH-Y2IBWcda4JmOcmNYxVj-Gf714n0",
-  authDomain: "react-journal-app-testin-a17f8.firebaseapp.com",
-  projectId: "react-journal-app-testin-a17f8",
-  storageBucket: "react-journal-app-testin-a17f8.firebasestorage.app",
-  messagingSenderId: "345096008229",
-  appId: "1:345096008229:web:3d183c334593dbba6bc5cc",
+  apiKey: VITE_APIKEY,
+  authDomain: VITE_AUTHDOMAIN,
+  projectId: VITE_PROJECTID,
+  storageBucket: VITE_STORAGEBUCKET,
+  messagingSenderId: VITE_MESSAGINGSENDERID,
+  appId: VITE_APPID,
 };
 
 
