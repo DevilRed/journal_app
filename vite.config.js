d13e1b4977f59402b868e6dbc67b24e3248fecb7
Christian <chrisdaq@gmail.com>
2024-11-18 12:03:43 -0400

    config env variables for vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { config } from "dotenv";
+const env = config({ path: ".env.testing" });
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,9 +9,10 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./src/setupTests.js",
+    setupFiles: "./setupTests.js",
     // you might want to disable it, if you don't have tests that rely on CSS
     // since parsing CSS is slow
     css: false,
+    env: env,
   },
 });
